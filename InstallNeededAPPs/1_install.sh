ce7ad1bf25c8d74d1240abdf553483f7938643b5
luebbo <l@myterhell.de>
2025-10-20 13:50:16 +0200

    251020

--- InstallNeededAPPs/1_install.sh
+++ InstallNeededAPPs/1_install.sh
@@ -1,1 +1,43 @@
 sudo apt install git
+#!/bin/bash
+command=(
+  "sudo snap refresh"
+  "sudo apt-get update"
+  "sudo apt-get -y upgrade"
+)
+SECONDS=0
+for i in "${command[@]}"
+do
+  echo ""
+  echo "perform: $i"
+  eval $i || echo failed: $i
+  #eval "$1"
+done
+echo ""
+duration=$SECONDS
+echo "time for updates needed:"
+echo "$((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
+echo ""
+
+
+apps=(
+  "git"
+  "docker"
+  "net-tools"
+  "iptables"
+  "ca-certificates"
+  "curl"
+)
+
+SECONDS=0
+for i in "${apps[@]}"
+do
+  echo ""
+  echo "perform: sudo apt-get -y install $i"
+  eval sudo apt-get -y install $i || echo failed: sudo apt-get -y install $i
+done
+echo ""
+duration=$SECONDS
+echo "time for installing apps needed:"
+echo "$((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
+echo ""
