f8591a54a55d018c3845d94d3f6bed072c50891d
luebbo <l@myterhell.de>
2025-10-31 06:28:00 +0100

    251031

--- InstallNeededAPPs/2_configure.sh
+++ InstallNeededAPPs/2_configure.sh
@@ -10,5 +10,5 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-udo usermod -aG docker,dialout,plugdev $USER
+sudo usermod -aG docker,dialout,plugdev $USER
 
