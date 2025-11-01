ca98d68e7eb8fb26b38f15e3c5f08c0239450788
luebbo <L@myterhell.de>
2025-11-01 05:05:48 +0100

    011125

--- InstallNeededAPPs/1_install.sh
+++ InstallNeededAPPs/1_install.sh
@@ -13,8 +13,25 @@ command=(
   "sudo apt-get -y upgrade"
 )
 SECONDS=0
+
+# for Docker GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo usermod -aG docker,dialout,plugdev $USER
+
 echo copy files to etc
-sudo cp -v ../FilesToCopy/etc /etc
+sudo cp -vr ../FilesToCopy/etc /etc
+echo copy ssh config
+sudo cp -v ../FilesToCopy/home/.ssh/config ~/.ssh/
+
 for i in "${command[@]}"
 do
   echo ""
--- InstallNeededAPPs/3_setup NDIS interface.sh
+++ InstallNeededAPPs/3_setup NDIS interface.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+sudo clear
+echo -en "\033[1;1H"
 searchfor=enx001122334455
 gotit=$(eval ip address|grep -o "$searchfor"|wc -l)
 # gotit="(($gotit))"
 while [ ! $gotit -eq 1 ]
 do
+    sudo echo -en "\033[1;1H"
     echo "Connect USB from Seer USB CDP to this device"
     echo "Power off the TCU, wait and power TCU on"
-    echo "waiting for 2 seconds ...."
+    echo "waiting for 1 seconds ...."
     echo  ""
     sleep 1
     # founds=eval ip address|grep -o "$searchfor"|wc -l
@@ -15,6 +18,8 @@ do
 done
 sudo ip address add 198.18.1.2/24 dev enx001122334455
 sudo ip link set enx001122334455 up
+sudo ifconfig | grep -A1 enx001122334455
+
 
 
 
