712098315ebac986346bf522cd33060513608165
luebbo <L@myterHell.de>
2025-11-04 13:58:03 +0100

    041125

--- ACU6examples/HelloACU6prepare.sh
+++ ACU6examples/HelloACU6prepare.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-ffffffffff
+PathOfAppToCopy=~/ACU6/FilesToCopy/ACU6apps/HelloACU6/
+TargetPathForApp=~/ACU6apps
+if [ ! -d "$TargetPathForApp" ]; then
+  echo "Directory doesn't exists. $TargetPathForApp"
+  echo "creating $TargetPathForApp"
+  mkdir $TargetPathForApp
+fi
+cp -rv $PathOfAppToCopy $TargetPathForApp
+
