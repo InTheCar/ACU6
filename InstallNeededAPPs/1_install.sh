7f2ce24dc894998aba919293684f812b494eb3d8
Luebbo <101571038+InTheCar@users.noreply.github.com>
2026-01-17 16:00:25 +0100

    Update 1_install.sh
    Update 1_install.sh

--- InstallNeededAPPs/1_install.sh
+++ InstallNeededAPPs/1_install.sh
@@ -28,7 +28,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt install apt-transport-https curl
+sudo apt -y install apt-transport-https curl
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
